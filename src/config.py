f76303ab98dc6ff78a8d21d40b60ecb5bb6478b3
Chris Yang <ysm0622@gmail.com>
2018-05-10 20:53:06 +0900

    Edit: split size

--- src/config.py
+++ src/config.py
@@ -1,9 +1,9 @@
 class Config():
-		split = 16
+		split = 128
 		
 		train_ratio = 0.8
 
 		crop_size = 32
 
 		# record_filename = 'data/Records/{data_type}-{index}.tfrecords'
-		record_filename = 'data/Records/data.tfrecords'
+		# record_filename = 'data/Records/data.tfrecords'
